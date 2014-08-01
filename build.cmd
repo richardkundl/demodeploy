84561e40e891899ba21d464b2adb8e0503a7b553
Richard Kundl <kundl.richard@gmail.com>
2014-08-01 17:05:58 +0200

    build.cmd added configuration target

--- build.cmd
+++ build.cmd
@@ -1,3 +1,9 @@
 @echo Off
 
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild src/Build.proj /nologo /m /v:m /t:Compile  /p:VisualStudioVersion=12.0;CONFIGURATION="debug"  /flp:LogFile="results/msbuild.log" /tv:4.0 /v:m
+set configuration=%1
+if "%configuration%" == "" (
+   set configuration=release
+)
+
+
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild src/Build.proj /nologo /m /v:m /t:Compile  /p:VisualStudioVersion=12.0;CONFIGURATION="%configuration%" /flp:LogFile="results/msbuild.log" /tv:4.0 /v:m
