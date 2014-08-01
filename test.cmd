4acd9f5cc27b29cd8c0ace3642fc1d5d14994e81
Richard Kundl <kundl.richard@gmail.com>
2014-08-01 22:58:17 +0200

    running tests

--- test.cmd
+++ test.cmd
@@ -6,4 +6,4 @@ if "%configuration%" == "" (
 )
 
 
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild src/Build.proj /nologo /m /v:m /t:Tests  /p:VisualStudioVersion=12.0;CONFIGURATION="%configuration%" /flp:LogFile="results/msbuild.log" /tv:4.0 /v:m
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild src/Build.proj /nologo /m /v:m /t:Tests  /p:VisualStudioVersion=12.0;CONFIGURATION="%configuration%";TESTS_CATEGORY="UnitTest" /flp:LogFile="results/msbuild.log" /tv:4.0 /v:m
