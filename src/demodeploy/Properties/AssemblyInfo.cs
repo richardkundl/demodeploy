6ef88b6212c2dabb388dfe6df7107049320ca1b0
Richard Kundl <kundl.richard@gmail.com>
2014-08-02 19:43:15 +0200

    update assemlyinfo.cs

--- src/demodeploy/Properties/AssemblyInfo.cs
+++ src/demodeploy/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("demodeploy")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("demodeploy")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCompany("Acme Co.")]
+[assembly: AssemblyProduct("Demo deploy")]
+[assembly: AssemblyCopyright("Copyright © 2014 Kundl Richard, All Rights Reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.12.1")]
+[assembly: AssemblyFileVersion("1.1.12.1")]
