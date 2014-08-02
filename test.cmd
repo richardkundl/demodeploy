f2813af5a19936140f69f6747a32b74fc91edd23
Richard Kundl <kundl.richard@gmail.com>
2014-08-02 10:28:38 +0200

    rename "Tests" target to "MsTests"

--- test.cmd
+++ test.cmd
@@ -5,4 +5,4 @@ if "%configuration%" == "" (
    set configuration=debug
 )
 
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild src/Build.proj /nologo /m /v:m /t:Tests  /p:VisualStudioVersion=12.0;CONFIGURATION="%configuration%";TESTS_CATEGORY="UnitTest|RegressionTest" /flp:LogFile="results/msbuild.log" /tv:4.0 /v:m
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild src/Build.proj /nologo /m /v:m /t:MsTests  /p:VisualStudioVersion=12.0;CONFIGURATION="%configuration%";TESTS_CATEGORY="UnitTest|RegressionTest" /flp:LogFile="results/msbuild.log" /tv:4.0 /v:m
