932cff678209fead16b52fe2e1a418e405b107d1
Richard Kundl <kundl.richard@gmail.com>
2014-08-15 11:24:30 +0200

    please, work

--- src/demodeploy.Admin/Properties/AssemblyInfo.cs
+++ src/demodeploy.Admin/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.1.12.1540")]
-[assembly: AssemblyFileVersion("1.1.12.1540")]
+[assembly: AssemblyVersion("1.1.12.1123")]
+[assembly: AssemblyFileVersion("1.1.12.1123")]
