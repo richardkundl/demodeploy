eae2d5838d2c389cea06095b01fed5985d0c246e
Richard Kundl <kundl.richard@gmail.com>
2014-08-15 11:22:30 +0200

    new demo website project for the multiple publish

---
+++ src/demodeploy.Admin/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="demodeploy.Admin.MvcApplication" Language="C#" %>
---
+++ src/demodeploy.Admin/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("demodeploy.Admin")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Acme Co.")]
+[assembly: AssemblyProduct("Demo deploy")]
+[assembly: AssemblyCopyright("Copyright © 2014 Kundl Richard, All Rights Reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d8a50146-402e-40e7-9c8a-0be4c7c7984f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.1.12.1540")]
+[assembly: AssemblyFileVersion("1.1.12.1540")]
